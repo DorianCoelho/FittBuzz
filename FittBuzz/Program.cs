25fbd0fc7174b960a9fa7eed15a3bd13b205bd8f
Dorian Brites <d.brites@sucasuc.com>
2023-09-25 00:08:08 +0200

    first commit

---
+++ FittBuzz/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FittBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fitt");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FITTBUZZ");
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
